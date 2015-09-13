939fe0ff1749c5844532ff43431014f390abe84f
jmarcelos <jmarcelos@gmail.com>
2015-09-13 14:03:45 -0300

    refactor do sitemap para tornar os métodos testaveis

--- scrapper/sitemap.py
+++ scrapper/sitemap.py
@@ -27,46 +27,46 @@ class SitemapReader:
         self.sitemaps = sitemaps
 
     def run(self):
+        full_homepage_list = []
         for sitemap_key in self.sitemaps:
-            opener = urllib2.build_opener()
-            opener.addheaders = [self.USER_AGENT]
+            url = self.sitemaps[sitemap_key]
+            localizacoes = self.searchHomeProduct(url)
+            homepage_list = self.readSitemap(localizacoes, sitemap_key)
+            full_homepage_list.append(homepage_list)
 
-            file = opener.open(self.sitemaps[sitemap_key])
-            file_content = file.read()
-            file.close()
-            xmldoc = minidom.parseString(file_content)
+        print len(full_homepage_list)
+        return full_homepage_list
 
-            localizacoes = xmldoc.getElementsByTagName('loc')
+    def readSitemap(self, localizacoes, sitemap_key):
+        homepage_list = []
+        for local in localizacoes:
+            url = local.firstChild.nodeValue
+            if "xml" in url:
+                homepage_list = self.readSitemap(self.searchHomeProduct(url), sitemap_key)
+            else:
+                conteudo = Sitemap(url=url, prioridade=10, site=sitemap_key)
+                #print conteudo.to_dict()
+                homepage_list.append(conteudo.to_dict())
 
-            for local in localizacoes:
+        return homepage_list
 
-                url = local.firstChild.nodeValue
-                if "xml" in url:
-                    opener = urllib2.build_opener()
-                    opener.addheaders = [self.USER_AGENT]
+    def searchHomeProduct(self, url):
+        opener = urllib2.build_opener()
+        opener.addheaders = [self.USER_AGENT]
+        file = opener.open(url)
+        file_content = file.read()
+        file.close()
+        xmldoc = minidom.parseString(file_content)
 
-                    file = opener.open(url)
-                    file_content = file.read()
-                    file.close()
-                    xmldoc = minidom.parseString(file_content)
+        localizacoes = xmldoc.getElementsByTagName('loc')
 
-                    localizacoes = xmldoc.getElementsByTagName('loc')
-                    for local in localizacoes:
-
-                        url = local.firstChild.nodeValue
-                        conteudo = Sitemap(url=url, prioridade=10, site=sitemap_key)
-                        print conteudo.to_dict()
-                else:
-                    conteudo = Sitemap(url=url, prioridade=10, site=sitemap_key)
-                    print conteudo.to_dict()
-
-                #conteudo.save()
+        return localizacoes
 
     def sameSitemapContent(self, content):
 
         return false
 
-
+#x = SitemapReader({"Extra":"http://buscando.extra.com.br/sitemap.xml" })
 x = SitemapReader({"Americanas":"http://www.americanas.com.br/sitemap_index_acom.xml", "Extra":"http://buscando.extra.com.br/sitemap.xml" })
 #x = SitemapReader({"Americanas":"http://www.americanas.com.br/sitemap_index_acom.xml" })
-x.run()
+print x.run()
